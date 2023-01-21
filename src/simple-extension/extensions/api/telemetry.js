ead0882b8007b2bcb695851655b55424168197c5
Dan Jarvis <danwakeem@gmail.com>
2023-01-21 08:45:58 -0600

    Added telemetry API listener

---
+++ src/simple-extension/extensions/api/telemetry.js
@@ -0,0 +1,86 @@
+const http = require('http');
+const { EventEmitter } = require('node:events')
+
+const TelemetryAPIService = ({
+  extensionIdentifier
+}) => {
+  const baseUrl = `http://${process.env.AWS_LAMBDA_RUNTIME_API}/2022-07-01/telemetry`;
+  const keepAliveAgent = new http.Agent({ keepAlive: true });
+  const sandboxHostname = 'sandbox';
+  const telemetryEventEmitter = new EventEmitter();
+  const completeEvent = 'telemetry-complete';
+  let server;
+
+  const startServer = () => {
+    server = http
+      .createServer((request, response) => {
+        if (request.method !== 'POST') throw new Error('Unexpected request method');
+
+        let body = '';
+        request.on('data', (data) => {
+          body += data;
+        });
+        request.on('end', () => {
+          response.writeHead(200, {});
+          response.end('OK');
+          const data = JSON.parse(body);
+          console.log(JSON.stringify(data));
+
+          for (const event of data) {
+            if (event.type === 'platform.runtimeDone') {
+              telemetryEventEmitter.emit(completeEvent);
+              break;
+            }
+          }
+        });
+      })
+      .listen(4243, sandboxHostname)
+  }
+
+  const registerTelemetry = () => new Promise((resolve, reject) => {
+    const eventTypes = ['platform', 'function'];
+    const putData = JSON.stringify({
+      destination: { protocol: 'HTTP', URI: `http://${sandboxHostname}:4243` },
+      types: eventTypes,
+      buffering: { timeoutMs: 25, maxBytes: 262144, maxItems: 1000 },
+      schemaVersion: '2022-07-01',
+    });
+    const request = http.request(
+      baseUrl,
+      {
+        agent: keepAliveAgent,
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Lambda-Extension-Identifier': extensionIdentifier,
+          'Content-Length': Buffer.byteLength(putData),
+        },
+      },
+      (response) => {
+        if (response.statusCode === 200) {
+          resolve();
+        } else {
+          reject(
+            new Error(`Unexpected logs subscribe response status code: ${response.statusCode}`)
+          );
+        }
+      }
+    );
+    request.on('error', reject);
+    request.write(putData);
+    request.end();
+  });
+
+  const waitForPlatformDoneEvent = async () => new Promise((resolve) => {
+    telemetryEventEmitter.on(completeEvent, resolve);
+  })
+
+  return {
+    server,
+    registerTelemetry,
+    startServer,
+    waitForPlatformDoneEvent,
+  };
+};
+
+module.exports = TelemetryAPIService;
--- src/simple-extension/extensions/index.js
+++ src/simple-extension/extensions/index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-// The #!/usr/bin/env node is important so that the lambda function knows we are trying to
-// run our extension using nodejs.
+// The #!/usr/bin/env node is important so that our lambda function
+// knows we are trying to run our extension using nodejs.
 
 const { EventTypes, ...extensionAPIService} = require('./api/extensions')();
+const TelemetryAPIService = require('./api/telemetry');
 const logger = require('./util/logger');
 
 // We use a self invoking function so that our extension code will boot up on lambda start
@@ -12,27 +13,41 @@ const logger = require('./util/logger');
   logger.log('Extension is booting up...');
   const extensionIdentifier = await extensionAPIService.register();
 
-  // extensionIdentifier is how lambda knows which
-  // extension is sending each event
+  const telemetryAPIService = TelemetryAPIService({
+    extensionIdentifier,
+  });
+  // Start up our telemetry local server
+  telemetryAPIService.startServer();
+  // Register our telemetry server with the Telemetry API
+  telemetryAPIService.registerTelemetry();
+
+  // extensionIdentifier is how lambda knows which extension is sending each event
   logger.log('Extension Id received: ', extensionIdentifier);
 
   while(true) {
     /**
-     * We call the next event twice on each lambda invocation.
-     * The first call lets lambda know that the extension is ready to begin.
+     * The first call to lets lambda know that the extension is ready to begin.
      * The second call lets lambda know that our extension has finished running.
      * 
-     * Keep in mind the lambda function freezes execution immediately after the second call to next.
-     * This means that when our lambda performs a warm start we will begin execution again starting
-     * with a response from extensionAPIService.next and then freezing again after we make this call again
-     * on the next iteration of the while loop.
+     * Keep in mind the lambda function freezes execution immediately after
+     * the second call to next.
+     *
+     * This means that when our lambda performs a warm start we will begin
+     * execution again starting with a response from extensionAPIService.next
+     * and then freezing again after we make this call again on the next
+     * iteration of the while loop.
      */
     logger.log('Extension calling next: ', extensionIdentifier);
     const event = await extensionAPIService.next({
       extensionIdentifier
     });
 
-    // Do whatever work we want the extension to do
+    // Wait for our telemetry server to receive the platform done event.
+    // The platform.runtimeDone event lets us know when our lambda runtime
+    // code has completed its execution.
+    await telemetryAPIService.waitForPlatformDoneEvent();
+
+    // Do some more work before we let our extension complete execution
     logger.log('Extension doing some work: ', extensionIdentifier);
     await extensionAPIService.doSomeWork();
 
